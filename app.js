4e8516f15ff4be3e4148bdddb01315f940477138
nayon117 <hasibulhasannayon260@gmail.com>
2023-08-30 11:33:02 +0600

    completed footer with js functionality

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const loadData = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
     const data = await res.json();
-    const catagories = data.data.news_category.slice(0,3);
-    // console.log(catagories);
+    const catagories = data.data.news_category;
+    console.log(catagories);
+     
 
     const tabContainer = document.getElementById("tab-container")
     // loop for accessing element from the array 
 
-    catagories.forEach(category => {
+catagories.forEach(category => {
         const div = document.createElement("div");
         div.innerHTML = `
         <a onclick="handleLoadNews('${category.category_id}')"  class="tab tab-active">${category.category_name}</a>
@@ -62,7 +63,7 @@ const handleLoadNews = async (categoryId) => {
               </div>
             </div>
             <div class="card-detaild-btn">
-              <button
+              <button onclick="handleModal('${news._id}')"
                 class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
               >
                 Details
@@ -75,5 +76,32 @@ const handleLoadNews = async (categoryId) => {
         cardContainer.appendChild(div)
     })
 }
+
+const handleModal =async (newsId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
+    const data = await res.json();
+    console.log(data.data[0]);
+    const newsI = data.data[0]
+    const modalContainer = document.getElementById("modal-container")
+    
+        const div = document.createElement("div");
+    div.innerHTML = `
+    <dialog id="my_modal_1" class="modal">
+   <form method="dialog" class="modal-box">
+   <img src= ${newsI.image_url}>
+    <h3 class="font-bold text-lg">${newsI.title}</h3>
+    <p class="py-4">${newsI.details.slice(0,70)}</p>
+    <div class="modal-action">
+      <!-- if there is a button in form, it will close the modal -->
+      <button class="btn">Close</button>
+    </div>
+  </form>
+</dialog>`;
+    modalContainer.appendChild(div)
+    const modal = document.getElementById("my_modal_1");
+    modal.showModal()
+    
+}
+
 handleLoadNews("01")
 loadData();
